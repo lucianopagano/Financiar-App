e717b3f61d73e046820285f354be321afdbfe4a2
Alvaro Pagano <alvapagano819@gmail.com>
2023-08-01 17:50:57 -0300

    eliminacion de botones del menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -35,36 +35,6 @@ const appPages: AppPage[] = [
     url: '/folder/Alta-Impuesto',
     iosIcon: documentTextOutline,
     mdIcon: documentTextOutline
-  },
-  {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
   }
 ];
 
