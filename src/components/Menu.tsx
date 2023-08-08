90e23d872aa989a76780c19df458f659baa29ec4
Alvaro Pagano <alvapagano819@gmail.com>
2023-08-08 12:54:17 -0300

    edicion cabecera menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -47,8 +47,8 @@ const Menu: React.FC = () => {
     <IonMenu contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
+          <IonListHeader>Financiar-App</IonListHeader>
+          <IonNote>alvapagano819@gmail.com</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
